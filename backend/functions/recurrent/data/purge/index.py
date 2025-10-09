fbe63907607bacbd380cc0b1a657bc7d0237b358
unknownfeature <unknownfeature@unknownfeature>
2025-10-09 15:26:26 -0400

    imports and variable names fixes

--- backend/functions/recurrent/data/purge/index.py
+++ backend/functions/recurrent/data/purge/index.py
@@ -1,32 +1,32 @@
 import json
-import json
 from datetime import datetime, timezone
 
-import boto3
 from dateutil.relativedelta import relativedelta
 from sqlalchemy import delete
 
+from backend.lib import constants
 from backend.lib.db import begin_session, Data
 
-def handler(event, context):
+
+def handler(event, _):
     session = begin_session()
     try:
 
         three_months_ago = datetime.now(timezone.utc) - relativedelta(months=3)
         cutoff_timestamp = int(three_months_ago.timestamp())
 
-        print(f"Deleting data older than: {three_months_ago.isoformat()} ({cutoff_timestamp})")
+        print(f'Deleting data older than: {three_months_ago.isoformat()} ({cutoff_timestamp})')
 
         delete_stmt = delete(Data).where(Data.time < cutoff_timestamp)
 
         result = session.execute(delete_stmt)
         session.commit()
 
-        print(f"Successfully deleted { result.rowcount} rows.")
+        print(f'Successfully deleted {result.rowcount} rows.')
 
         return {
-            constants.statusCode: 200,
-            constants.body: json.dumps(f"Deleted { result.rowcount} rows.")
+            constants.status_code: 200,
+            constants.body: json.dumps(f'Deleted {result.rowcount} rows.')
         }
 
 
--- backend/functions/recurrent/occurrence/purge/index.py
+++ backend/functions/recurrent/occurrence/purge/index.py
@@ -4,6 +4,7 @@ from datetime import datetime, timezone
 from dateutil.relativedelta import relativedelta
 from sqlalchemy import delete
 
+from backend.lib import constants
 from backend.lib.db import begin_session, Data, Occurrence
 
 
@@ -14,18 +15,18 @@ def handler(event, _):
         three_months_ago = datetime.now(timezone.utc) - relativedelta(months=3)
         cutoff_timestamp = int(three_months_ago.timestamp())
 
-        print(f"Deleting data older than: {three_months_ago.isoformat()} ({cutoff_timestamp})")
+        print(f'Deleting data older than: {three_months_ago.isoformat()} ({cutoff_timestamp})')
 
         delete_stmt = delete(Occurrence).where(Data.time < cutoff_timestamp)
 
         result = session.execute(delete_stmt)
         session.commit()
 
-        print(f"Successfully deleted { result.rowcount} rows.")
+        print(f'Successfully deleted {result.rowcount} rows.')
 
         return {
-            constants.statusCode: 200,
-            constants.body: json.dumps(f"Deleted { result.rowcount} rows.")
+            constants.status_code: 200,
+            constants.body: json.dumps(f'Deleted {result.rowcount} rows.')
         }
 
 
